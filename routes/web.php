c293afcc950973b2737c060e7a121198840fa270
MohamedMarei <mohamedmarei2115@gmail.com>
2021-04-08 01:33:45 +0200

    Revert "Edit Admin Panel"
    This reverts commit 943349306d92495abc8d45e0e00c82906a616880.

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/auth/login');
+    return view('welcome');
 });
 
 Auth::routes();
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
